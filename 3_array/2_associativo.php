6f7200452492077da257c2dae7f9b88f3900c65d
ELISANDRA-COSTA <108496873+ELISANDRA-COSTA@users.noreply.github.com>
2022-07-07 21:10:57 -0300

    Add files via upload

---
+++ 3_array/2_associativo.php
@@ -0,0 +1,16 @@
+<?php
+ 
+$pessoa = array();
+
+$pessoa['nome']= 'elisandra cabral costa';
+$pessoa['email']= 'elisandra.costa@escola.pr.gov.br';
+$pessoa['telefone']= '(44) 99474209'; 
+
+echo $pessoa['nome'];
+echo '<br>';
+echo $pessoa['ameil'];
+echo '<br>';
+echo $pessoa['telefone'];
+ 
+ 
+?>
